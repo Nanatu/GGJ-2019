d8bc0323dd4833434153954851a91056d0697eed
William Norris <emcskid@gmail.com>
2019-01-26 11:47:30 -0600

    Updated CameraControl to damp movement toward player - WZN

--- GGJ-2019Project/Assets/Scripts/CameraController.cs
+++ GGJ-2019Project/Assets/Scripts/CameraController.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject player;
+    private Vector2 velocity;
 
-    private Vector3 offset;
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        offset = transform.position - player.transform.position;
-
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
-        
-        transform.position = player.transform.position + offset;
+
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x,ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y,ref velocity.y, smoothTimeY);
+    
+        transform.position = new Vector3(posX, posY, transform.position.z);
 
     }
 }
+    
