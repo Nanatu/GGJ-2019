2f197c1f422731230478720a3b7a959c6761ba28
Richard McNeil <rimcnei@gmail.com>
2019-01-26 13:30:00 -0600

    Adding tree generating seed Adding Tree ages as it grows

---
+++ GGJ-2019Project/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    
+    public List<GameObject> resources;
+    public GameObject seed;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
