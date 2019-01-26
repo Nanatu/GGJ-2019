714c5514ea23b70a71da2d4bb115dce2e80b1c65
Daniel <daniel.stoughton@gmail.com>
2019-01-26 10:39:53 -0600

    Added breath meter script

---
+++ GGJ-2019Project/Assets/Scripts/BreathMeter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreathMeter : MonoBehaviour
+{
+    public float SecondsOfBreath;
+    private float maxBreath;
+    private bool safeAtHome = false;
+
+    void Start()
+    {
+        maxBreath = SecondsOfBreath;
+    }
+
+    void Update()
+    {
+        if (!safeAtHome)
+            SecondsOfBreath -= Time.deltaTime;
+        else
+        {
+            if (SecondsOfBreath < maxBreath)
+            {
+                SecondsOfBreath += Time.deltaTime * 2;
+            }
+        }
+
+        if (SecondsOfBreath > maxBreath)
+            SecondsOfBreath = maxBreath;
+    }
+
+    private void OnTriggerEnter2d(Collider2D other)
+    {
+        safeAtHome = other.tag == "Oasis";
+    }
+
+    private void OnTriggerExit2d(Collider2D other)
+    {
+        safeAtHome = !(other.tag == "Oasis");
+    }
+}
