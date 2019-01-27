2d75860de35e03e0912916e40fc905625985d0d1
Richard McNeil <rimcnei@gmail.com>
2019-01-26 18:24:49 -0600

    Added Resource Manager Prefab Resource Manager will spawn objects on start

--- GGJ-2019Project/Assets/Scripts/ResourceManager.cs
+++ GGJ-2019Project/Assets/Scripts/ResourceManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
@@ -7,12 +8,34 @@ public class ResourceManager : MonoBehaviour
     
     public List<GameObject> resources;
     public GameObject seed;
+    private List<GameObject> spawnedResources;
+
+    public int startingResources = 30;
+    
+    //set custom range for random position
+    public float MinX = 0;
+    public float MaxX = 10;
+    public float MinY = 0;
+    public float MaxY = 10;
+ 
+    //for 3d you have z position
+    public float MinZ = 0;
+    public float MaxZ = 10;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnedResources = new List<GameObject>();
+        int resourceCount = resources.Count;
+        for (int i = 0; i < startingResources; i++)
+        {
+            int randomResource = Random.Range(0, resourceCount -1);
+            Vector3 randomLocation = GenerateRandomLocation2D();
+            GameObject newResource = Instantiate(resources[randomResource], randomLocation, Quaternion.identity);
+            newResource.transform.parent = gameObject.transform;
+            spawnedResources.Add(newResource);
+        }
     }
 
     // Update is called once per frame
@@ -20,4 +43,22 @@ public class ResourceManager : MonoBehaviour
     {
         
     }
+
+    private Vector3 GenerateRandomLocation3D()
+    {
+        float x = Random.Range(MinX,MaxX);
+        float y = Random.Range(MinY,MaxY);
+        float z = Random.Range(MinZ,MaxZ);
+        
+        return new Vector3(x,y,z);
+    }
+    
+    private Vector3 GenerateRandomLocation2D()
+    {
+        float x = Random.Range(MinX,MaxX);
+        float y = Random.Range(MinY,MaxY);
+        
+        return new Vector3(x,y,0);
+    }
+   
 }
